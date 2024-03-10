17ff5dec83728fa9c52353dd8677d76fcf7af3a6
Jaylen Garner <jaylen@moonraydevelopment.com>
2024-03-09 20:29:48 -0500

    Created a ValidateWord function to form a response object based on the response of the dictionary API

--- backend/routes/words.js
+++ backend/routes/words.js
@@ -3,6 +3,26 @@ require("dotenv").config();
 const express = require("express");
 const router = express.Router();
 
+const validateWord = (data) => {
+  const maxRecommendations = 3;
+  const response = {};
+
+  if (!data.length) {
+    response.error = "Please enter a valid word.";
+  } else {
+    const [firstItem, ...restItems] = data;
+    if (typeof firstItem === "string") {
+      response.recommendations = restItems.slice(
+        0,
+        Math.min(restItems.length, maxRecommendations)
+      );
+    } else {
+      response.data = firstItem;
+    }
+  }
+  return response;
+};
+
 // Get word data from the Dictionary API
 router.post("/", async (req, res) => {
   const { word } = req.body;
@@ -12,7 +32,8 @@ router.post("/", async (req, res) => {
   );
 
   const data = await response.json();
-  res.json({ data: data });
+  const validatedData = validateWord(data);
+  res.json(validatedData);
 });
 
 module.exports = router;
