469a0b9923101db753785e1cac253fc996b23cb0
Jaylen Garner <jaylen@moonraydevelopment.com>
2024-03-09 19:26:59 -0500

    Configured a route to validate a word with the Dictionary API

--- backend/routes/words.js
+++ backend/routes/words.js
@@ -1,10 +1,18 @@
-const express = require("express");
+require("dotenv").config();
 
+const express = require("express");
 const router = express.Router();
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  res.json({ mssg: "Request recieved" });
+// Get word data from the Dictionary API
+router.post("/", async (req, res) => {
+  const { word } = req.body;
+
+  const response = await fetch(
+    `https://www.dictionaryapi.com/api/v3/references/sd2/json/${word}?key=${process.env.DICTIONARY_API_KEY}`
+  );
+
+  const data = await response.json();
+  res.json({ data: data });
 });
 
 module.exports = router;
