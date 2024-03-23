1de45988b2f27ba2cb81bbb3e0ed88c953a155dd
Jaylen Garner <jaylen@moonraydevelopment.com>
2024-03-23 11:30:19 -0400

    Created a basic navigation bar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Pages & Components
+import Nav from "./components/Nav";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Nav />
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
---
+++ frontend/src/components/Nav.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <header>
+      <div className="container">
+        <Link to="/">Home</Link>
+      </div>
+    </header>
+  );
+};
+
+export default Nav;
