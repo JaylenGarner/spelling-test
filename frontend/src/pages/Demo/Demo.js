42d5755d11fcd6199ad2385de847df33f07915db
Jaylen Garner <jaylen@moonraydevelopment.com>
2024-04-06 12:29:54 -0400

    Hide the form upon submission, dynamically display the word that was selected.

--- frontend/src/pages/Demo/Demo.js
+++ frontend/src/pages/Demo/Demo.js
@@ -3,24 +3,31 @@ import { useState } from "react";
 
 const Demo = () => {
   const [word, setWord] = useState("");
+  const [submitted, setSubmitted] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("Selected Word: " + word);
+    setSubmitted(true);
   };
 
   return (
     <div className={styles.container}>
       <h1>Spelling Test Demo</h1>
-      <h2>Enter a word</h2>
-      <form className={styles.container} onSubmit={(e) => handleSubmit(e)}>
-        <input
-          type="text"
-          value={word}
-          onChange={(e) => setWord(e.target.value)}
-        />
-        <button> Submit</button>
-      </form>
+
+      {!submitted ? (
+        <form className={styles.container} onSubmit={(e) => handleSubmit(e)}>
+          <h2>Enter a word</h2>
+          <input
+            type="text"
+            value={word}
+            onChange={(e) => setWord(e.target.value)}
+          />
+          <button> Submit</button>
+        </form>
+      ) : (
+        <h2>Selected Word: {word}</h2>
+      )}
     </div>
   );
 };
