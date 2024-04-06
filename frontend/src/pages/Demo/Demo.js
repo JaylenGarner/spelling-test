dd6c8abf864a34e4753beeb42d4ba3fcd7eb8bc6
Jaylen Garner <jaylen@moonraydevelopment.com>
2024-04-06 12:24:42 -0400

    Implemented state management for the form and a submission handler function.

--- frontend/src/pages/Demo/Demo.js
+++ frontend/src/pages/Demo/Demo.js
@@ -1,12 +1,24 @@
 import styles from "./Demo.module.css";
+import { useState } from "react";
 
 const Demo = () => {
+  const [word, setWord] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Selected Word: " + word);
+  };
+
   return (
     <div className={styles.container}>
       <h1>Spelling Test Demo</h1>
       <h2>Enter a word</h2>
-      <form className={styles.container}>
-        <input type="text" />
+      <form className={styles.container} onSubmit={(e) => handleSubmit(e)}>
+        <input
+          type="text"
+          value={word}
+          onChange={(e) => setWord(e.target.value)}
+        />
         <button> Submit</button>
       </form>
     </div>
