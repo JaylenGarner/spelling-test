6aba309dbb643a7548a0c7fd3e7797dcee41fbc3
Jaylen Garner <jaylen@moonraydevelopment.com>
2024-04-13 09:27:28 -0400

    Styled the demo test form

--- frontend/src/components/DemoTest/DemoTest.js
+++ frontend/src/components/DemoTest/DemoTest.js
@@ -1,8 +1,11 @@
 import { useState } from "react";
+import styles from "./DemoTest.module.css";
 
 const DemoTest = () => {
+  const [submitted, setSubmitted] = useState(false);
+
   return (
-    <form>
+    <form className={styles.container}>
       <h2>Spell the word</h2>
       <input type="text" />
       <button>Submit</button>
